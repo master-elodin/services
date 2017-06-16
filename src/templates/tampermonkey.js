587f149eafe872f9e1d150c191bef30c35582ca3
master-elodin <timothy.vandoren@gmail.com>
2017-06-16 05:27:34 -0500

    Fix tampermonkey @match to @include

--- src/templates/tampermonkey.js
+++ src/templates/tampermonkey.js
@@ -4,7 +4,7 @@
 // @updateURL    https://confluence-tools.swacorp.com/pages/viewpage.action?pageId=340558277
 // @description  ${description}
 // @author       ${author}
-// @match        ${target}
+// @include      ${target}
 ${externalDependencies}
 // ==/UserScript==
 (function() {
