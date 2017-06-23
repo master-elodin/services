79191c15e1b85c1417203a9421e49ac0c21ded7e
master-elodin <timothy.vandoren@gmail.com>
2017-06-23 09:10:27 -0500

    Group selection now working

--- src/page/page.js
+++ src/page/page.js
@@ -32,9 +32,6 @@ function Page() {
         return !!instance.activeHostGroup() && !instance.editMode();
     });
 
-    instance.pageData = new Item();
-    instance.pageData.scriptVersion = SCRIPT_VERSION;
-
     instance.activeApp = ko.observable();
     instance.activeEnv = ko.observable();
     instance.activeHostGroup = ko.observable();
@@ -55,19 +52,42 @@ function Page() {
         }
     }
 
+    instance.pageData = new Item({name: "configuration", childrenTypes: "applications", applications: []});
+    instance.pageData.scriptVersion = SCRIPT_VERSION;
     var getSettingsAsJsonText = function() {
         return JSON.stringify(instance.pageData.export());
     };
 
     instance.save = function() {
-        localStorage.setItem(Page.DATA_NAME, getSettingsAsJsonText());
+        var settingsAsJsonText = getSettingsAsJsonText();
+        localStorage.setItem(Page.DATA_NAME, settingsAsJsonText);
+        console.log("Saved page data as JSON", instance.pageData.export(), settingsAsJsonText);
     };
 
     instance.load = function() {
-        var existingData = localStorage.getItem(Page.DATA_NAME);
+        var existingData = JSON.parse(localStorage.getItem(Page.DATA_NAME) || "{}");
         if(existingData) {
-            instance.pageData.import(JSON.parse(existingData));
+            // backward compatibility
+            if(existingData.scriptVersion === "1.0.1") {
+                console.log("Converting script version 1.0.1 to " + SCRIPT_VERSION);
+                existingData.childrenType = Item.ChildrenTypes.APP;
+                existingData.name = "configuration";
+                existingData.applications.forEach(function(app) {
+                    app.childrenType = Item.ChildrenTypes.ENV;
+                    app.environments.forEach(function(env) {
+                        env.childrenType = Item.ChildrenTypes.HOST_GROUP;
+                        env.hostGroups.forEach(function(hostGroup) {
+                            hostGroup.childrenType = Item.ChildrenTypes.HOST;
+                        });
+                    });
+                });
+            }
+            console.log("Loading data", existingData);
+            instance.pageData.import(existingData);
+            console.log("Page data after load", instance.pageData.export());
         } else {
+            console.log("Tried to load but no valid existing data found", existingData);
+            console.log("Using page data:", instance.pageData.export());
             instance.editMode(true);
         }
     };
